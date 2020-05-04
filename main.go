f4a691eefcf16bc77665b0ef1cc66f87c6767696
Oleg Adianov <oleg.adianov@gmail.com>
2020-05-05 00:48:20 +0300

    Переделал с использованием flag, добавил флаг дебага -d и сделал, что бы можно было из командной строки передавать сразу несколько url-ов

--- main.go
+++ main.go
@@ -2,29 +2,25 @@ package main
 
 import (
 	"bufio"
-	"fmt"
+	"flag"
 	"log"
 	"os"
-	"path/filepath"
 )
 
 var DEBUG bool
 
 func main() {
-	var dl dnldr
+	var dl DownStruct
 
 	// дебагинг сообщений
-	DEBUG = false
+	flag.BoolVar(&DEBUG, "d", false, "for debug messages")
 
 	// в качестве параметра принимаем либо url, либо ключ
-	if len(os.Args) == 2 && (os.Args[1] == "-h" || os.Args[1] == "--help") {
-		fmt.Printf("Use: %s <url>\n\n", filepath.Base(os.Args[0]))
-		os.Exit(1)
-	}
+	flag.Parse()
 
 	var phurl string
 	// если запущено без параметров - читаем url из ввода
-	if len(os.Args) < 2 {
+	if len(flag.Args()) < 1 {
 		scanner := bufio.NewScanner(os.Stdin)
 		for {
 			ok := scanner.Scan()
@@ -46,15 +42,15 @@ func main() {
 			}
 		}
 	} else {
-		// если задан - из аргументов строки
-		phurl = os.Args[1]
+		// если заданы url-ы в ком строке, то итерируемся по ним
+		for _, phurl = range flag.Args() {
 
-		if err := dl.getParam(phurl); err != nil {
-			log.Printf("Error: %v\n", err)
-		}
-		// закачка
-		if err := dl.download(); err != nil {
-			log.Printf("Error: %v\n", err)
+			if err := dl.getParam(phurl); err != nil {
+				log.Printf("Error: %v\n", err)
+			} else if err := dl.download(); err != nil {
+				// закачка
+				log.Printf("Error: %v\n", err)
+			}
 		}
 	}
 }
